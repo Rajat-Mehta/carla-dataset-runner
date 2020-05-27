d02fd2a73753e365c0054151987d90e715bc7d5e
rajat-mehta <rajatmehta1992@gmail.com>
2020-05-27 14:50:45 +0200

    updated gitignore and carla egg path

--- settings.py
+++ settings.py
@@ -1,1 +1,1 @@
-CARLA_EGG_PATH = "/home/rajat/Desktop/Carla 0.9.8/PythonAPI/carla/dist/carla-0.9.8-py3.5-linux-x86_64.egg"
+CARLA_EGG_PATH = "/home/rajat/Desktop/Carla/CARLA_0.9.8/PythonAPI/carla/dist/carla-0.9.8-py3.5-linux-x86_64.egg"
